2e41118c0b85d4f86e4f2450f53de503f0d186e2
Kogepan25629 <73163994+Kogepan25629@users.noreply.github.com>
2023-04-15 14:46:01 +0900

    Add exmaple elf binary

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod elf;
 
 fn main() {
     let memory: Box<u8> = Box::new(200);
-    elf::load("".to_string(), memory);
-    println!("Hello, world!");
+    elf::load(
+        "D:/Desktop/VSCode/Koge29_H8-300H_Emulator/example/example1.elf".to_string(),
+        memory,
+    );
 }
