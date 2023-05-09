87a684290fa02a111ed99f45357d2e0a7e16846c
Kogepan25629 <73163994+Kogepan25629@users.noreply.github.com>
2023-05-09 15:58:40 +0900

    Change resister access method

--- src/cpu.rs
+++ src/cpu.rs
@@ -9,58 +9,6 @@ pub struct Cpu<'a> {
     er: [u32; 8],
 }
 
-#[derive(Debug, Clone, Copy)]
-enum Resister8 {
-    R0H,
-    R1H,
-    R2H,
-    R3H,
-    R4H,
-    R5H,
-    R6H,
-    R7H,
-    R0L,
-    R1L,
-    R2L,
-    R3L,
-    R4L,
-    R5L,
-    R6L,
-    R7L,
-}
-
-#[derive(Debug, Clone, Copy)]
-enum Resister16 {
-    E0,
-    E1,
-    E2,
-    E3,
-    E4,
-    E5,
-    E6,
-    E7,
-    R0,
-    R1,
-    R2,
-    R3,
-    R4,
-    R5,
-    R6,
-    R7,
-}
-
-#[derive(Debug, Clone, Copy)]
-enum Resister32 {
-    ER0,
-    ER1,
-    ER2,
-    ER3,
-    ER4,
-    ER5,
-    ER6,
-    ER7,
-}
-
 impl<'a> Cpu<'a> {
     pub fn new(mcu: &'a Mcu) -> Self {
         Cpu {
--- src/cpu/addressing_mode.rs
+++ src/cpu/addressing_mode.rs
@@ -14,46 +14,46 @@ impl std::fmt::Display for AddressingError {
 }
 
 impl<'a> Cpu<'a> {
-    fn write_rn8(&mut self, register: Resister8, value: u8) -> Result<(), AddressingError> {
-        match register as usize {
+    fn write_rn8(&mut self, register_field: u8, value: u8) -> Result<(), AddressingError> {
+        match register_field {
             // R0H..=R7H
             0..=7 => {
-                self.er[register as usize] =
-                    (self.er[register as usize] & 0xffff00ff) | ((value as u32) << 8)
+                self.er[register_field as usize] =
+                    (self.er[register_field as usize] & 0xffff00ff) | ((value as u32) << 8)
             }
             // R0L..=R7L
             8..=15 => {
-                self.er[register as usize - 8] =
-                    (self.er[register as usize - 8] & 0xffffff00) | (value as u32)
+                self.er[register_field as usize - 8] =
+                    (self.er[register_field as usize - 8] & 0xffffff00) | (value as u32)
             }
             _ => return Err(AddressingError),
         };
         Ok(())
     }
 
-    fn read_rn8(&self, register: Resister8) -> Result<u8, AddressingError> {
-        match register as usize {
+    fn read_rn8(&self, register_field: u8) -> Result<u8, AddressingError> {
+        match register_field as usize {
             // R0H..=R7H
-            0..=7 => return Ok((self.er[register as usize] >> 8) as u8),
+            0..=7 => return Ok((self.er[register_field as usize] >> 8) as u8),
             // R0L..=R7L
-            8..=15 => return Ok((self.er[register as usize - 8]) as u8),
+            8..=15 => return Ok((self.er[register_field as usize - 8]) as u8),
             _ => return Err(AddressingError),
         };
     }
 
-    fn write_rn16(&self, register: Resister16, value: u16) -> Result<(), AddressingError> {
+    fn write_rn16(&self, register_field: u8, value: u16) -> Result<(), AddressingError> {
         Ok(())
     }
 
-    fn read_rn16(&self, register: Resister16) -> Result<u16, AddressingError> {
+    fn read_rn16(&self, register_field: u8) -> Result<u16, AddressingError> {
         Ok(0)
     }
 
-    fn write_rn32(&self, register: Resister32, value: u32) -> Result<(), AddressingError> {
+    fn write_rn32(&self, register_field: u8, value: u32) -> Result<(), AddressingError> {
         Ok(())
     }
 
-    fn read_rn32(&self, register: Resister32) -> Result<u32, AddressingError> {
+    fn read_rn32(&self, register_field: u8) -> Result<u32, AddressingError> {
         Ok(0)
     }
 }
@@ -66,16 +66,16 @@ mod tests {
     fn test_write_rn8() {
         let mcu = Mcu::new();
         let mut cpu = Cpu::new(&mcu);
-        cpu.write_rn8(crate::cpu::Resister8::R0H, 0xff).unwrap();
-        cpu.write_rn8(crate::cpu::Resister8::R7H, 0xff).unwrap();
+        cpu.write_rn8(0b0000, 0xff).unwrap(); // R0H
+        cpu.write_rn8(0b0111, 0xff).unwrap(); // R7H
         assert_eq!(cpu.er[0], 0x0000ff00);
         assert_eq!(cpu.er[7], 0x0000ff00);
 
         cpu.er[0] = 0;
         cpu.er[7] = 0;
 
-        cpu.write_rn8(crate::cpu::Resister8::R0L, 0xff).unwrap();
-        cpu.write_rn8(crate::cpu::Resister8::R7L, 0xff).unwrap();
+        cpu.write_rn8(0b1000, 0xff).unwrap();
+        cpu.write_rn8(0b1111, 0xff).unwrap();
         assert_eq!(cpu.er[0], 0x000000ff);
         assert_eq!(cpu.er[7], 0x000000ff);
     }
@@ -86,14 +86,14 @@ mod tests {
         let mut cpu = Cpu::new(&mcu);
         cpu.er[0] = 0x0000ff00;
         cpu.er[7] = 0x0000ff00;
-        assert_eq!(cpu.read_rn8(crate::cpu::Resister8::R0H).unwrap(), 0xff);
-        assert_eq!(cpu.read_rn8(crate::cpu::Resister8::R1H).unwrap(), 0x00);
-        assert_eq!(cpu.read_rn8(crate::cpu::Resister8::R7H).unwrap(), 0xff);
+        assert_eq!(cpu.read_rn8(0b0000).unwrap(), 0xff); // R0H
+        assert_eq!(cpu.read_rn8(0b0001).unwrap(), 0x00); // R1H
+        assert_eq!(cpu.read_rn8(0b0111).unwrap(), 0xff); // R7H
 
         cpu.er[0] = 0x000000ff;
         cpu.er[7] = 0x000000ff;
-        assert_eq!(cpu.read_rn8(crate::cpu::Resister8::R0L).unwrap(), 0xff);
-        assert_eq!(cpu.read_rn8(crate::cpu::Resister8::R1L).unwrap(), 0x00);
-        assert_eq!(cpu.read_rn8(crate::cpu::Resister8::R7L).unwrap(), 0xff);
+        assert_eq!(cpu.read_rn8(0b1000).unwrap(), 0xff); // R0L
+        assert_eq!(cpu.read_rn8(0b1001).unwrap(), 0x00); // R1L
+        assert_eq!(cpu.read_rn8(0b1111).unwrap(), 0xff); // R7L
     }
 }
