a3c8d4844faf3fbb3b81d965cde5433d23457c85
Kogepan25629 <73163994+Kogepan25629@users.noreply.github.com>
2023-05-13 15:04:58 +0900

    Rename cr to ccr in cpu

--- src/cpu.rs
+++ src/cpu.rs
@@ -5,7 +5,7 @@ mod addressing_mode;
 pub struct Cpu<'a> {
     mcu: &'a mut Mcu,
     pc: u32,
-    cr: u8,
+    ccr: u8,
     er: [u32; 8],
 }
 
@@ -14,7 +14,7 @@ impl<'a> Cpu<'a> {
         Cpu {
             mcu: mcu,
             pc: 0,
-            cr: 0,
+            ccr: 0,
             er: [0; 8],
         }
     }
