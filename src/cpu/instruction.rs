75c0fc5ac29cca65160da85e773abe8f04306c2c
Kogepan25629 <73163994+Kogepan25629@users.noreply.github.com>
2023-05-18 15:01:33 +0900

    Fix get_nibble_opcode

--- src/cpu/instruction.rs
+++ src/cpu/instruction.rs
@@ -6,7 +6,7 @@ impl<'a> Cpu<'a> {
     // order: 1 ~ 4 [0x1234]
     pub fn get_nibble_opcode(opcode: u16, order: u8) -> u8 {
         match order {
-            1..=4 => (opcode >> (4 * (4 - order))) as u8,
+            1..=4 => ((opcode >> (4 * (4 - order))) as u8) & 0xf,
             _ => panic!("invalud order"),
         }
     }
