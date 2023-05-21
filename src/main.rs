cb68567c02bf496fe3cdb604687f2bc26412f034
Kogepan25629 <73163994+Kogepan25629@users.noreply.github.com>
2023-05-21 15:51:56 +0900

    Add options in command line arguments

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,33 @@ mod mcu;
 mod memory;
 mod setting;
 
+use clap::Parser;
+
 use crate::cpu::Cpu;
 use mcu::Mcu;
 
+#[derive(Parser)]
+struct Args {
+    /// path of the elf file to execute
+    #[arg(short, long)]
+    elf: String,
+
+    /// Print executed opcode
+    #[arg(long)]
+    debug_instruction: Option<bool>,
+}
+
 fn main() {
+    let args = Args::parse();
+    if let Some(v) = args.debug_instruction {
+        *setting::ENABLE_PRINT_OPCODE.write().unwrap() = v;
+    }
+
     let mut mcu = Mcu::new();
     let mut cpu = Cpu::new(&mut mcu);
 
     //let mut memory: Memory = create_memory();
-    elf::load(
-        "D:/Desktop/VSCode/Koge29_H8-300H_Emulator/example/example3.elf".to_string(),
-        &mut cpu,
-    );
+    elf::load(args.elf, &mut cpu);
     // print_memory(&cpu.mcu.memory);
 
     cpu.run().unwrap();
