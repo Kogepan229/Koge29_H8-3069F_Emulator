de233dfbdcb53b0352631b0db526f64bd0f11e8f
Kogepan25629 <kogepan25629@gmail.com>
2023-06-22 23:16:06 +0900

    Add test for subs

--- src/cpu/instruction/subs.rs
+++ src/cpu/instruction/subs.rs
@@ -32,3 +32,164 @@ impl Cpu {
         f()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::cpu::Cpu;
+
+    #[test]
+    fn test_subs1() {
+        let mut cpu = Cpu::new();
+        cpu.bus.memory[0..2].copy_from_slice(&[0x1b, 0x00]);
+        cpu.write_rn_l(0, 10).unwrap();
+        let opcode = cpu.fetch();
+        let state = cpu.exec(opcode).unwrap();
+        assert_eq!(state, 2);
+        assert_eq!(cpu.ccr, 0);
+        assert_eq!(cpu.read_rn_l(0).unwrap(), 9);
+
+        // register 7
+        let mut cpu = Cpu::new();
+        cpu.bus.memory[0..2].copy_from_slice(&[0x1b, 0x07]);
+        cpu.write_rn_l(7, 10).unwrap();
+        let opcode = cpu.fetch();
+        let state = cpu.exec(opcode).unwrap();
+        assert_eq!(state, 2);
+        assert_eq!(cpu.ccr, 0);
+        assert_eq!(cpu.read_rn_l(7).unwrap(), 9);
+
+        // overflow
+        let mut cpu = Cpu::new();
+        cpu.bus.memory[0..2].copy_from_slice(&[0x1b, 0x00]);
+        cpu.write_rn_l(0, std::i32::MIN as u32).unwrap();
+        let opcode = cpu.fetch();
+        let state = cpu.exec(opcode).unwrap();
+        assert_eq!(state, 2);
+        assert_eq!(cpu.ccr, 0);
+        assert_eq!(cpu.read_rn_l(0).unwrap(), std::i32::MAX as u32);
+
+        // u32 MAX - 1
+        let mut cpu = Cpu::new();
+        cpu.bus.memory[0..2].copy_from_slice(&[0x1b, 0x00]);
+        cpu.write_rn_l(0, std::u32::MAX).unwrap();
+        let opcode = cpu.fetch();
+        let state = cpu.exec(opcode).unwrap();
+        assert_eq!(state, 2);
+        assert_eq!(cpu.ccr, 0);
+        assert_eq!(cpu.read_rn_l(0).unwrap(), std::u32::MAX - 1);
+
+        // 0 - 1
+        let mut cpu = Cpu::new();
+        cpu.bus.memory[0..2].copy_from_slice(&[0x1b, 0x00]);
+        cpu.write_rn_l(0, 0).unwrap();
+        let opcode = cpu.fetch();
+        let state = cpu.exec(opcode).unwrap();
+        assert_eq!(state, 2);
+        assert_eq!(cpu.ccr, 0);
+        assert_eq!(cpu.read_rn_l(0).unwrap(), -1i32 as u32);
+    }
+
+    #[test]
+    fn test_subs2() {
+        let mut cpu = Cpu::new();
+        cpu.bus.memory[0..2].copy_from_slice(&[0x1b, 0x80]);
+        cpu.write_rn_l(0, 10).unwrap();
+        let opcode = cpu.fetch();
+        let state = cpu.exec(opcode).unwrap();
+        assert_eq!(state, 2);
+        assert_eq!(cpu.ccr, 0);
+        assert_eq!(cpu.read_rn_l(0).unwrap(), 8);
+
+        // register 7
+        let mut cpu = Cpu::new();
+        cpu.bus.memory[0..2].copy_from_slice(&[0x1b, 0x87]);
+        cpu.write_rn_l(7, 10).unwrap();
+        let opcode = cpu.fetch();
+        let state = cpu.exec(opcode).unwrap();
+        assert_eq!(state, 2);
+        assert_eq!(cpu.ccr, 0);
+        assert_eq!(cpu.read_rn_l(7).unwrap(), 8);
+
+        // overflow
+        let mut cpu = Cpu::new();
+        cpu.bus.memory[0..2].copy_from_slice(&[0x1b, 0x80]);
+        cpu.write_rn_l(0, std::i32::MIN as u32).unwrap();
+        let opcode = cpu.fetch();
+        let state = cpu.exec(opcode).unwrap();
+        assert_eq!(state, 2);
+        assert_eq!(cpu.ccr, 0);
+        assert_eq!(cpu.read_rn_l(0).unwrap(), std::i32::MAX as u32 - 1);
+
+        // u32 MAX - 1
+        let mut cpu = Cpu::new();
+        cpu.bus.memory[0..2].copy_from_slice(&[0x1b, 0x80]);
+        cpu.write_rn_l(0, std::u32::MAX).unwrap();
+        let opcode = cpu.fetch();
+        let state = cpu.exec(opcode).unwrap();
+        assert_eq!(state, 2);
+        assert_eq!(cpu.ccr, 0);
+        assert_eq!(cpu.read_rn_l(0).unwrap(), std::u32::MAX - 2);
+
+        // 0 - 1
+        let mut cpu = Cpu::new();
+        cpu.bus.memory[0..2].copy_from_slice(&[0x1b, 0x80]);
+        cpu.write_rn_l(0, 0).unwrap();
+        let opcode = cpu.fetch();
+        let state = cpu.exec(opcode).unwrap();
+        assert_eq!(state, 2);
+        assert_eq!(cpu.ccr, 0);
+        assert_eq!(cpu.read_rn_l(0).unwrap(), -2i32 as u32);
+    }
+
+    #[test]
+    fn test_subs4() {
+        let mut cpu = Cpu::new();
+        cpu.bus.memory[0..2].copy_from_slice(&[0x1b, 0x90]);
+        cpu.write_rn_l(0, 10).unwrap();
+        let opcode = cpu.fetch();
+        let state = cpu.exec(opcode).unwrap();
+        assert_eq!(state, 2);
+        assert_eq!(cpu.ccr, 0);
+        assert_eq!(cpu.read_rn_l(0).unwrap(), 6);
+
+        // register 7
+        let mut cpu = Cpu::new();
+        cpu.bus.memory[0..2].copy_from_slice(&[0x1b, 0x97]);
+        cpu.write_rn_l(7, 10).unwrap();
+        let opcode = cpu.fetch();
+        let state = cpu.exec(opcode).unwrap();
+        assert_eq!(state, 2);
+        assert_eq!(cpu.ccr, 0);
+        assert_eq!(cpu.read_rn_l(7).unwrap(), 6);
+
+        // overflow
+        let mut cpu = Cpu::new();
+        cpu.bus.memory[0..2].copy_from_slice(&[0x1b, 0x90]);
+        cpu.write_rn_l(0, std::i32::MIN as u32).unwrap();
+        let opcode = cpu.fetch();
+        let state = cpu.exec(opcode).unwrap();
+        assert_eq!(state, 2);
+        assert_eq!(cpu.ccr, 0);
+        assert_eq!(cpu.read_rn_l(0).unwrap(), std::i32::MAX as u32 - 3);
+
+        // u32 MAX - 1
+        let mut cpu = Cpu::new();
+        cpu.bus.memory[0..2].copy_from_slice(&[0x1b, 0x90]);
+        cpu.write_rn_l(0, std::u32::MAX).unwrap();
+        let opcode = cpu.fetch();
+        let state = cpu.exec(opcode).unwrap();
+        assert_eq!(state, 2);
+        assert_eq!(cpu.ccr, 0);
+        assert_eq!(cpu.read_rn_l(0).unwrap(), std::u32::MAX - 4);
+
+        // 0 - 1
+        let mut cpu = Cpu::new();
+        cpu.bus.memory[0..2].copy_from_slice(&[0x1b, 0x90]);
+        cpu.write_rn_l(0, 0).unwrap();
+        let opcode = cpu.fetch();
+        let state = cpu.exec(opcode).unwrap();
+        assert_eq!(state, 2);
+        assert_eq!(cpu.ccr, 0);
+        assert_eq!(cpu.read_rn_l(0).unwrap(), -4i32 as u32);
+    }
+}
