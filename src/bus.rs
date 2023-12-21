c7dffb92cf7e3795877de36bd16aae6146544abd
Kogepan25629 <kogepan25629@gmail.com>
2023-12-21 21:28:18 +0900

    Add DRAM and area0 ~ area7 address

--- src/bus.rs
+++ src/bus.rs
@@ -3,6 +3,39 @@ use anyhow::{bail, Result};
 
 const EXCEPTION_HANDLING_VENCTOR_SIZE: usize = 0xff;
 
+pub const AREA0_START_ADDR: u32 = 0;
+pub const AREA0_END_ADDR: u32 = 0x1fffff;
+pub const AREA0_SIZE: usize = (AREA0_END_ADDR - AREA0_START_ADDR + 1) as usize;
+
+pub const AREA1_START_ADDR: u32 = 0x200000;
+pub const AREA1_END_ADDR: u32 = 0x3fffff;
+pub const AREA1_SIZE: usize = (AREA1_END_ADDR - AREA1_START_ADDR + 1) as usize;
+
+// DRAM
+pub const AREA2_START_ADDR: u32 = 0x400000;
+pub const AREA2_END_ADDR: u32 = 0x5fffff;
+pub const AREA2_SIZE: usize = (AREA2_END_ADDR - AREA2_START_ADDR + 1) as usize;
+
+pub const AREA3_START_ADDR: u32 = 0x600000;
+pub const AREA3_END_ADDR: u32 = 0x7fffff;
+pub const AREA3_SIZE: usize = (AREA3_END_ADDR - AREA3_START_ADDR + 1) as usize;
+
+pub const AREA4_START_ADDR: u32 = 0x800000;
+pub const AREA4_END_ADDR: u32 = 0x9fffff;
+pub const AREA4_SIZE: usize = (AREA4_END_ADDR - AREA4_START_ADDR + 1) as usize;
+
+pub const AREA5_START_ADDR: u32 = 0xa00000;
+pub const AREA5_END_ADDR: u32 = 0xbfffff;
+pub const AREA5_SIZE: usize = (AREA5_END_ADDR - AREA5_START_ADDR + 1) as usize;
+
+pub const AREA6_START_ADDR: u32 = 0xc00000;
+pub const AREA6_END_ADDR: u32 = 0xdfffff;
+pub const AREA6_SIZE: usize = (AREA6_END_ADDR - AREA6_START_ADDR + 1) as usize;
+
+pub const AREA7_START_ADDR: u32 = 0xe00000;
+pub const AREA7_END_ADDR: u32 = 0xffffff;
+pub const AREA7_SIZE: usize = (AREA7_END_ADDR - AREA7_START_ADDR + 1) as usize;
+
 pub const IO_REGISTERS1_START_ADDR: u32 = 0xfee000;
 pub const IO_REGISTERS1_END_ADDR: u32 = 0xfee0bf;
 pub const IO_REGISTERS1_SIZE: usize =
@@ -20,6 +53,7 @@ pub const IO_REGISTERS2_EMC1_SIZE: usize =
 pub struct Bus {
     pub memory: Memory,
     pub exception_handling_vector: [u8; EXCEPTION_HANDLING_VENCTOR_SIZE],
+    dram: [u8; AREA2_SIZE],
     io_registrs1: [u8; IO_REGISTERS1_SIZE],
     io_registrs2: [u8; IO_REGISTERS2_EMC1_SIZE],
 }
@@ -29,6 +63,7 @@ impl Bus {
         Bus {
             memory: create_memory(),
             exception_handling_vector: [0; EXCEPTION_HANDLING_VENCTOR_SIZE],
+            dram: [0; AREA2_SIZE],
             io_registrs1: [0; IO_REGISTERS1_SIZE],
             io_registrs2: [0; IO_REGISTERS2_EMC1_SIZE],
         }
@@ -39,6 +74,9 @@ impl Bus {
             0x00..=0xff => self.exception_handling_vector[addr as usize] = value,
             IO_REGISTERS1_START_ADDR..=IO_REGISTERS1_END_ADDR => {
                 self.io_registrs1[(addr - IO_REGISTERS1_START_ADDR) as usize] = value
+            }
+            AREA2_START_ADDR..=AREA2_END_ADDR => {
+                self.dram[(addr - AREA2_START_ADDR) as usize] = value
             }
             MEMORY_START_ADDR..=MEMORY_END_ADDR => {
                 self.memory[(addr - MEMORY_START_ADDR) as usize] = value
@@ -57,6 +95,9 @@ impl Bus {
             IO_REGISTERS1_START_ADDR..=IO_REGISTERS1_END_ADDR => {
                 println!("{:x}", addr);
                 return Ok(self.io_registrs1[(addr - IO_REGISTERS1_START_ADDR) as usize]);
+            }
+            AREA2_START_ADDR..=AREA2_END_ADDR => {
+                return Ok(self.dram[(addr - AREA2_START_ADDR) as usize])
             }
             MEMORY_START_ADDR..=MEMORY_END_ADDR => {
                 return Ok(self.memory[(addr - MEMORY_START_ADDR) as usize])
