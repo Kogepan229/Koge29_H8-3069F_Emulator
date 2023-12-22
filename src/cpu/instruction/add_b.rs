5942c201d6aa6e114a716301d7654453517ba622
Kogepan25629 <kogepan25629@gmail.com>
2023-12-22 15:27:23 +0900

    ADD.B

--- src/cpu/instruction/add_b.rs
+++ src/cpu/instruction/add_b.rs
@@ -1,11 +1,11 @@
-use crate::cpu::{Cpu, CCR};
+use crate::cpu::{Cpu, StateType, CCR};
 use anyhow::{bail, Result};
 
 impl Cpu {
-    pub(in super::super) fn add_b(&mut self, opcode: u16) -> Result<usize> {
+    pub(in super::super) async fn add_b(&mut self, opcode: u16) -> Result<u8> {
         match (opcode >> 8) as u8 {
-            0x80..=0x8f => return self.add_b_imm(opcode),
-            0x08 => return self.add_b_rn(opcode),
+            0x80..=0x8f => return self.add_b_imm(opcode).await,
+            0x08 => return self.add_b_rn(opcode).await,
             _ => bail!("invalid opcode [{:>04x}]", opcode),
         }
     }
@@ -45,27 +45,21 @@ impl Cpu {
         value as u8
     }
 
-    fn add_b_imm(&mut self, opcode: u16) -> Result<usize> {
-        let mut f = || -> Result<usize> {
-            let register = Cpu::get_nibble_opcode(opcode, 2)?;
-            let dest = self.read_rn_b(register)?;
-            let result = self.add_b_proc(dest, opcode as u8);
-            self.write_rn_b(register, result)?;
-            Ok(2)
-        };
-        f()
+    async fn add_b_imm(&mut self, opcode: u16) -> Result<u8> {
+        let register = Cpu::get_nibble_opcode(opcode, 2)?;
+        let dest = self.read_rn_b(register)?;
+        let result = self.add_b_proc(dest, opcode as u8);
+        self.write_rn_b(register, result)?;
+        Ok(self.calc_state(StateType::I, 1).await?)
     }
 
-    fn add_b_rn(&mut self, opcode: u16) -> Result<usize> {
-        let mut f = || -> Result<usize> {
-            let register_dest = Cpu::get_nibble_opcode(opcode, 4)?;
-            let dest = self.read_rn_b(register_dest)?;
-            let register_src = Cpu::get_nibble_opcode(opcode, 3)? & 0x7;
-            let src = self.read_rn_b(register_src)?;
-            let result = self.add_b_proc(dest, src);
-            self.write_rn_b(register_dest, result)?;
-            Ok(2)
-        };
-        f()
+    async fn add_b_rn(&mut self, opcode: u16) -> Result<u8> {
+        let register_dest = Cpu::get_nibble_opcode(opcode, 4)?;
+        let dest = self.read_rn_b(register_dest)?;
+        let register_src = Cpu::get_nibble_opcode(opcode, 3)? & 0x7;
+        let src = self.read_rn_b(register_src)?;
+        let result = self.add_b_proc(dest, src);
+        self.write_rn_b(register_dest, result)?;
+        Ok(self.calc_state(StateType::I, 1).await?)
     }
 }
