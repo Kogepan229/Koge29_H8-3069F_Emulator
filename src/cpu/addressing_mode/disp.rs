7ac75e6bafa0f8083251d7ea808a309722f7d583
Kogepan229 <kogepan25629@gmail.com>
2024-06-30 15:37:12 +0900

    disp

--- src/cpu/addressing_mode/disp.rs
+++ src/cpu/addressing_mode/disp.rs
@@ -6,13 +6,7 @@ impl Cpu {
         let addr = self.read_rn_l(register_field)?;
         let result = addr
             .checked_add_signed((disp as i16) as i32)
-            .ok_or_else(|| {
-                anyhow!(
-                    "attempt to add with overflow [{:x} + {:x}]",
-                    addr,
-                    disp as i16
-                )
-            })?
+            .ok_or_else(|| anyhow!("attempt to add with overflow [{:x} + {:x}]", addr, disp as i16))?
             & 0x00ffffff;
         Ok(result)
     }
@@ -24,143 +18,65 @@ impl Cpu {
         } else {
             let result = addr
                 .checked_add_signed((0xff000000 + disp) as i32)
-                .ok_or_else(|| {
-                    anyhow!(
-                        "attempt to add with overflow [{:x} + {:x}]",
-                        addr,
-                        (0xffff0000 + disp)
-                    )
-                })?
+                .ok_or_else(|| anyhow!("attempt to add with overflow [{:x} + {:x}]", addr, (0xffff0000 + disp)))?
                 & 0x00ffffff;
             Ok(result)
         }
     }
 
-    pub(in super::super) async fn write_disp16_ern_b(
-        &mut self,
-        register_field: u8,
-        disp: u16,
-        value: u8,
-    ) -> Result<()> {
-        self.write_abs24_b(self.get_addr_disp16(register_field, disp)?, value)
-            .await?;
+    pub(in super::super) fn write_disp16_ern_b(&mut self, register_field: u8, disp: u16, value: u8) -> Result<()> {
+        self.write_abs24_b(self.get_addr_disp16(register_field, disp)?, value)?;
         Ok(())
     }
 
-    pub(in super::super) async fn read_disp16_ern_b(
-        &self,
-        register_field: u8,
-        disp: u16,
-    ) -> Result<u8> {
-        Ok(self
-            .read_abs24_b(self.get_addr_disp16(register_field, disp)?)
-            .await?)
+    pub(in super::super) fn read_disp16_ern_b(&self, register_field: u8, disp: u16) -> Result<u8> {
+        Ok(self.read_abs24_b(self.get_addr_disp16(register_field, disp)?)?)
     }
 
-    pub(in super::super) async fn write_disp16_ern_w(
-        &mut self,
-        register_field: u8,
-        disp: u16,
-        value: u16,
-    ) -> Result<()> {
-        self.write_abs24_w(self.get_addr_disp16(register_field, disp)?, value)
-            .await?;
+    pub(in super::super) fn write_disp16_ern_w(&mut self, register_field: u8, disp: u16, value: u16) -> Result<()> {
+        self.write_abs24_w(self.get_addr_disp16(register_field, disp)?, value)?;
         Ok(())
     }
 
-    pub(in super::super) async fn read_disp16_ern_w(
-        &self,
-        register_field: u8,
-        disp: u16,
-    ) -> Result<u16> {
-        Ok(self
-            .read_abs24_w(self.get_addr_disp16(register_field, disp)?)
-            .await?)
+    pub(in super::super) fn read_disp16_ern_w(&self, register_field: u8, disp: u16) -> Result<u16> {
+        Ok(self.read_abs24_w(self.get_addr_disp16(register_field, disp)?)?)
     }
 
-    pub(in super::super) async fn write_disp16_ern_l(
-        &mut self,
-        register_field: u8,
-        disp: u16,
-        value: u32,
-    ) -> Result<()> {
-        self.write_abs24_l(self.get_addr_disp16(register_field, disp)?, value)
-            .await?;
+    pub(in super::super) fn write_disp16_ern_l(&mut self, register_field: u8, disp: u16, value: u32) -> Result<()> {
+        self.write_abs24_l(self.get_addr_disp16(register_field, disp)?, value)?;
         Ok(())
     }
 
-    pub(in super::super) async fn read_disp16_ern_l(
-        &self,
-        register_field: u8,
-        disp: u16,
-    ) -> Result<u32> {
-        Ok(self
-            .read_abs24_l(self.get_addr_disp16(register_field, disp)?)
-            .await?)
+    pub(in super::super) fn read_disp16_ern_l(&self, register_field: u8, disp: u16) -> Result<u32> {
+        Ok(self.read_abs24_l(self.get_addr_disp16(register_field, disp)?)?)
     }
 
     ////
-    pub(in super::super) async fn write_disp24_ern_b(
-        &mut self,
-        register_field: u8,
-        disp: u32,
-        value: u8,
-    ) -> Result<()> {
-        self.write_abs24_b(self.get_addr_disp24(register_field, disp)?, value)
-            .await?;
+    pub(in super::super) fn write_disp24_ern_b(&mut self, register_field: u8, disp: u32, value: u8) -> Result<()> {
+        self.write_abs24_b(self.get_addr_disp24(register_field, disp)?, value)?;
         Ok(())
     }
 
-    pub(in super::super) async fn read_disp24_ern_b(
-        &self,
-        register_field: u8,
-        disp: u32,
-    ) -> Result<u8> {
-        Ok(self
-            .read_abs24_b(self.get_addr_disp24(register_field, disp)?)
-            .await?)
+    pub(in super::super) fn read_disp24_ern_b(&self, register_field: u8, disp: u32) -> Result<u8> {
+        Ok(self.read_abs24_b(self.get_addr_disp24(register_field, disp)?)?)
     }
 
-    pub(in super::super) async fn write_disp24_ern_w(
-        &mut self,
-        register_field: u8,
-        disp: u32,
-        value: u16,
-    ) -> Result<()> {
-        self.write_abs24_w(self.get_addr_disp24(register_field, disp)?, value)
-            .await?;
+    pub(in super::super) fn write_disp24_ern_w(&mut self, register_field: u8, disp: u32, value: u16) -> Result<()> {
+        self.write_abs24_w(self.get_addr_disp24(register_field, disp)?, value)?;
         Ok(())
     }
 
-    pub(in super::super) async fn read_disp24_ern_w(
-        &self,
-        register_field: u8,
-        disp: u32,
-    ) -> Result<u16> {
-        Ok(self
-            .read_abs24_w(self.get_addr_disp24(register_field, disp)?)
-            .await?)
+    pub(in super::super) fn read_disp24_ern_w(&self, register_field: u8, disp: u32) -> Result<u16> {
+        Ok(self.read_abs24_w(self.get_addr_disp24(register_field, disp)?)?)
     }
 
-    pub(in super::super) async fn write_disp24_ern_l(
-        &mut self,
-        register_field: u8,
-        disp: u32,
-        value: u32,
-    ) -> Result<()> {
-        self.write_abs24_l(self.get_addr_disp24(register_field, disp)?, value)
-            .await?;
+    pub(in super::super) fn write_disp24_ern_l(&mut self, register_field: u8, disp: u32, value: u32) -> Result<()> {
+        self.write_abs24_l(self.get_addr_disp24(register_field, disp)?, value)?;
         Ok(())
     }
 
-    pub(in super::super) async fn read_disp24_ern_l(
-        &self,
-        register_field: u8,
-        disp: u32,
-    ) -> Result<u32> {
-        Ok(self
-            .read_abs24_l(self.get_addr_disp24(register_field, disp)?)
-            .await?)
+    pub(in super::super) fn read_disp24_ern_l(&self, register_field: u8, disp: u32) -> Result<u32> {
+        Ok(self.read_abs24_l(self.get_addr_disp24(register_field, disp)?)?)
     }
 }
 
@@ -168,157 +84,135 @@ impl Cpu {
 mod tests {
     use crate::cpu::Cpu;
 
-    #[tokio::test]
-    async fn test_write_disp16_ern_b() {
+    fn test_write_disp16_ern_b() {
         let mut cpu = Cpu::new();
         cpu.er[0] = 0xfff000;
-        cpu.write_disp16_ern_b(0, 0x0f10, 0xff).await.unwrap();
-        assert_eq!(cpu.read_abs24_b(0xffff10).await.unwrap(), 0xff);
+        cpu.write_disp16_ern_b(0, 0x0f10, 0xff).unwrap();
+        assert_eq!(cpu.read_abs24_b(0xffff10).unwrap(), 0xff);
 
         cpu.er[1] = 0xfffe0a;
-        cpu.write_disp16_ern_b(1, 0xfff6, 0xff).await.unwrap();
-        assert_eq!(cpu.read_abs24_b(0xfffe00).await.unwrap(), 0xff);
+        cpu.write_disp16_ern_b(1, 0xfff6, 0xff).unwrap();
+        assert_eq!(cpu.read_abs24_b(0xfffe00).unwrap(), 0xff);
     }
 
-    #[tokio::test]
-    async fn test_read_disp16_ern_b() {
+    fn test_read_disp16_ern_b() {
         let mut cpu = Cpu::new();
         cpu.er[0] = 0xfff000;
-        cpu.write_abs24_b(0xffff10, 0xff).await.unwrap();
-        assert_eq!(cpu.read_disp16_ern_b(0, 0x0f10).await.unwrap(), 0xff);
+        cpu.write_abs24_b(0xffff10, 0xff).unwrap();
+        assert_eq!(cpu.read_disp16_ern_b(0, 0x0f10).unwrap(), 0xff);
 
         cpu.er[1] = 0xfffe0a;
-        cpu.write_abs24_b(0xfffe00, 0xff).await.unwrap();
-        assert_eq!(cpu.read_disp16_ern_b(1, 0xfff6).await.unwrap(), 0xff);
+        cpu.write_abs24_b(0xfffe00, 0xff).unwrap();
+        assert_eq!(cpu.read_disp16_ern_b(1, 0xfff6).unwrap(), 0xff);
     }
 
-    #[tokio::test]
-    async fn test_write_disp16_ern_w() {
+    fn test_write_disp16_ern_w() {
         let mut cpu = Cpu::new();
         cpu.er[0] = 0xfff000;
-        cpu.write_disp16_ern_w(0, 0x0f10, 0x0fff).await.unwrap();
-        assert_eq!(cpu.read_abs24_w(0xffff10).await.unwrap(), 0x0fff);
+        cpu.write_disp16_ern_w(0, 0x0f10, 0x0fff).unwrap();
+        assert_eq!(cpu.read_abs24_w(0xffff10).unwrap(), 0x0fff);
 
         cpu.er[1] = 0xfffe0a;
-        cpu.write_disp16_ern_w(1, 0xfff6, 0x0fff).await.unwrap();
-        assert_eq!(cpu.read_abs24_w(0xfffe00).await.unwrap(), 0x0fff);
+        cpu.write_disp16_ern_w(1, 0xfff6, 0x0fff).unwrap();
+        assert_eq!(cpu.read_abs24_w(0xfffe00).unwrap(), 0x0fff);
     }
 
-    #[tokio::test]
-    async fn test_read_disp16_ern_w() {
+    fn test_read_disp16_ern_w() {
         let mut cpu = Cpu::new();
         cpu.er[0] = 0xfff000;
-        cpu.write_abs24_w(0xffff10, 0x0fff).await.unwrap();
-        assert_eq!(cpu.read_disp16_ern_w(0, 0x0f10).await.unwrap(), 0x0fff);
+        cpu.write_abs24_w(0xffff10, 0x0fff).unwrap();
+        assert_eq!(cpu.read_disp16_ern_w(0, 0x0f10).unwrap(), 0x0fff);
 
         cpu.er[1] = 0xfffe0a;
-        cpu.write_abs24_w(0xfffe00, 0x0fff).await.unwrap();
-        assert_eq!(cpu.read_disp16_ern_w(1, 0xfff6).await.unwrap(), 0x0fff);
+        cpu.write_abs24_w(0xfffe00, 0x0fff).unwrap();
+        assert_eq!(cpu.read_disp16_ern_w(1, 0xfff6).unwrap(), 0x0fff);
     }
 
-    #[tokio::test]
-    async fn test_write_disp16_ern_l() {
+    fn test_write_disp16_ern_l() {
         let mut cpu = Cpu::new();
         cpu.er[0] = 0xfff000;
-        cpu.write_disp16_ern_l(0, 0x0f10, 0x0f0fff0f).await.unwrap();
-        assert_eq!(cpu.read_abs24_l(0xffff10).await.unwrap(), 0x0f0fff0f);
+        cpu.write_disp16_ern_l(0, 0x0f10, 0x0f0fff0f).unwrap();
+        assert_eq!(cpu.read_abs24_l(0xffff10).unwrap(), 0x0f0fff0f);
 
         cpu.er[1] = 0xfffe0a;
-        cpu.write_disp16_ern_l(1, 0xfff6, 0x0f0fff0f).await.unwrap();
-        assert_eq!(cpu.read_abs24_l(0xfffe00).await.unwrap(), 0x0f0fff0f);
+        cpu.write_disp16_ern_l(1, 0xfff6, 0x0f0fff0f).unwrap();
+        assert_eq!(cpu.read_abs24_l(0xfffe00).unwrap(), 0x0f0fff0f);
     }
 
-    #[tokio::test]
-    async fn test_read_disp16_ern_l() {
+    fn test_read_disp16_ern_l() {
         let mut cpu = Cpu::new();
         cpu.er[0] = 0xfff000;
-        cpu.write_abs24_l(0xffff10, 0x0f0fff0f).await.unwrap();
-        assert_eq!(cpu.read_disp16_ern_l(0, 0x0f10).await.unwrap(), 0x0f0fff0f);
+        cpu.write_abs24_l(0xffff10, 0x0f0fff0f).unwrap();
+        assert_eq!(cpu.read_disp16_ern_l(0, 0x0f10).unwrap(), 0x0f0fff0f);
 
         cpu.er[1] = 0xfffe0a;
-        cpu.write_abs24_l(0xfffe00, 0x0f0fff0f).await.unwrap();
-        assert_eq!(cpu.read_disp16_ern_l(1, 0xfff6).await.unwrap(), 0x0f0fff0f);
+        cpu.write_abs24_l(0xfffe00, 0x0f0fff0f).unwrap();
+        assert_eq!(cpu.read_disp16_ern_l(1, 0xfff6).unwrap(), 0x0f0fff0f);
     }
 
-    #[tokio::test]
-    async fn test_write_disp24_ern_b() {
+    fn test_write_disp24_ern_b() {
         let mut cpu = Cpu::new();
         cpu.er[0] = 0xfff000;
-        cpu.write_disp24_ern_b(0, 0x000f10, 0xff).await.unwrap();
-        assert_eq!(cpu.read_abs24_b(0xffff10).await.unwrap(), 0xff);
+        cpu.write_disp24_ern_b(0, 0x000f10, 0xff).unwrap();
+        assert_eq!(cpu.read_abs24_b(0xffff10).unwrap(), 0xff);
 
         cpu.er[1] = 0xfffe0a;
-        cpu.write_disp24_ern_b(1, 0xfffff6, 0xff).await.unwrap();
-        assert_eq!(cpu.read_abs24_b(0xfffe00).await.unwrap(), 0xff);
+        cpu.write_disp24_ern_b(1, 0xfffff6, 0xff).unwrap();
+        assert_eq!(cpu.read_abs24_b(0xfffe00).unwrap(), 0xff);
     }
 
-    #[tokio::test]
-    async fn test_read_disp24_ern_b() {
+    fn test_read_disp24_ern_b() {
         let mut cpu = Cpu::new();
         cpu.er[0] = 0xfff000;
-        cpu.write_abs24_b(0xffff10, 0xff).await.unwrap();
-        assert_eq!(cpu.read_disp24_ern_b(0, 0x000f10).await.unwrap(), 0xff);
+        cpu.write_abs24_b(0xffff10, 0xff).unwrap();
+        assert_eq!(cpu.read_disp24_ern_b(0, 0x000f10).unwrap(), 0xff);
 
         cpu.er[1] = 0xfffe0a;
-        cpu.write_abs24_b(0xfffe00, 0xff).await.unwrap();
-        assert_eq!(cpu.read_disp24_ern_b(1, 0xfffff6).await.unwrap(), 0xff);
+        cpu.write_abs24_b(0xfffe00, 0xff).unwrap();
+        assert_eq!(cpu.read_disp24_ern_b(1, 0xfffff6).unwrap(), 0xff);
     }
 
-    #[tokio::test]
-    async fn test_write_disp24_ern_w() {
+    fn test_write_disp24_ern_w() {
         let mut cpu = Cpu::new();
         cpu.er[0] = 0xfff000;
-        cpu.write_disp24_ern_w(0, 0x000f10, 0x0fff).await.unwrap();
-        assert_eq!(cpu.read_abs24_w(0xffff10).await.unwrap(), 0x0fff);
+        cpu.write_disp24_ern_w(0, 0x000f10, 0x0fff).unwrap();
+        assert_eq!(cpu.read_abs24_w(0xffff10).unwrap(), 0x0fff);
 
         cpu.er[1] = 0xfffe0a;
-        cpu.write_disp24_ern_w(1, 0xfffff6, 0x0fff).await.unwrap();
-        assert_eq!(cpu.read_abs24_w(0xfffe00).await.unwrap(), 0x0fff);
+        cpu.write_disp24_ern_w(1, 0xfffff6, 0x0fff).unwrap();
+        assert_eq!(cpu.read_abs24_w(0xfffe00).unwrap(), 0x0fff);
     }
 
-    #[tokio::test]
-    async fn test_read_disp24_ern_w() {
+    fn test_read_disp24_ern_w() {
         let mut cpu = Cpu::new();
         cpu.er[0] = 0xfff000;
-        cpu.write_abs24_w(0xffff10, 0x0fff).await.unwrap();
-        assert_eq!(cpu.read_disp24_ern_w(0, 0x000f10).await.unwrap(), 0x0fff);
+        cpu.write_abs24_w(0xffff10, 0x0fff).unwrap();
+        assert_eq!(cpu.read_disp24_ern_w(0, 0x000f10).unwrap(), 0x0fff);
 
         cpu.er[1] = 0xfffe0a;
-        cpu.write_abs24_w(0xfffe00, 0x0fff).await.unwrap();
-        assert_eq!(cpu.read_disp24_ern_w(1, 0xfffff6).await.unwrap(), 0x0fff);
+        cpu.write_abs24_w(0xfffe00, 0x0fff).unwrap();
+        assert_eq!(cpu.read_disp24_ern_w(1, 0xfffff6).unwrap(), 0x0fff);
     }
 
-    #[tokio::test]
-    async fn test_write_disp24_ern_l() {
+    fn test_write_disp24_ern_l() {
         let mut cpu = Cpu::new();
         cpu.er[0] = 0xfff000;
-        cpu.write_disp24_ern_l(0, 0x000f10, 0x0f0fff0f)
-            .await
-            .unwrap();
-        assert_eq!(cpu.read_abs24_l(0xffff10).await.unwrap(), 0x0f0fff0f);
+        cpu.write_disp24_ern_l(0, 0x000f10, 0x0f0fff0f).unwrap();
+        assert_eq!(cpu.read_abs24_l(0xffff10).unwrap(), 0x0f0fff0f);
 
         cpu.er[1] = 0xfffe0a;
-        cpu.write_disp24_ern_l(1, 0xfffff6, 0x0f0fff0f)
-            .await
-            .unwrap();
-        assert_eq!(cpu.read_abs24_l(0xfffe00).await.unwrap(), 0x0f0fff0f);
+        cpu.write_disp24_ern_l(1, 0xfffff6, 0x0f0fff0f).unwrap();
+        assert_eq!(cpu.read_abs24_l(0xfffe00).unwrap(), 0x0f0fff0f);
     }
 
-    #[tokio::test]
-    async fn test_read_disp24_ern_l() {
+    fn test_read_disp24_ern_l() {
         let mut cpu = Cpu::new();
         cpu.er[0] = 0xfff000;
-        cpu.write_abs24_l(0xffff10, 0x0f0fff0f).await.unwrap();
-        assert_eq!(
-            cpu.read_disp24_ern_l(0, 0x000f10).await.unwrap(),
-            0x0f0fff0f
-        );
+        cpu.write_abs24_l(0xffff10, 0x0f0fff0f).unwrap();
+        assert_eq!(cpu.read_disp24_ern_l(0, 0x000f10).unwrap(), 0x0f0fff0f);
 
         cpu.er[1] = 0xfffe0a;
-        cpu.write_abs24_l(0xfffe00, 0x0f0fff0f).await.unwrap();
-        assert_eq!(
-            cpu.read_disp24_ern_l(1, 0xfffff6).await.unwrap(),
-            0x0f0fff0f
-        );
+        cpu.write_abs24_l(0xfffe00, 0x0f0fff0f).unwrap();
+        assert_eq!(cpu.read_disp24_ern_l(1, 0xfffff6).unwrap(), 0x0f0fff0f);
     }
 }
