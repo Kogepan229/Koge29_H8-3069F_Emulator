0fe2a790dad426e3afc16f68f34f244bf8c32709
Kogepan229 <kogepan25629@gmail.com>
2024-11-23 19:27:25 +0900

    Fix division by zero

--- src/cpu/instruction/divxu.rs
+++ src/cpu/instruction/divxu.rs
@@ -22,7 +22,7 @@ impl Cpu {
         }
 
         let quotient: u16 = if rs == 0 { 0 } else { rd / u16::from(rs) };
-        let remainder: u16 = if rd == 0 { 0 } else { rd % u16::from(rs) };
+        let remainder: u16 = if rs == 0 { 0 } else { rd % u16::from(rs) };
         let result = (remainder << 8) | (quotient & 0xff);
         self.write_rn_w(rd_i, result)?;
 
@@ -49,7 +49,7 @@ impl Cpu {
         }
 
         let quotient: u32 = if rs == 0 { 0 } else { rd / u32::from(rs) };
-        let remainder: u32 = if rd == 0 { 0 } else { rd % u32::from(rs) };
+        let remainder: u32 = if rs == 0 { 0 } else { rd % u32::from(rs) };
         let result = (remainder << 16) | (quotient & 0xffff);
         self.write_rn_l(rd_i, result)?;
 
