dc56a1546222ede5d108b22f7bd9b63fe836c50d
Kogepan229 <kogepan25629@gmail.com>
2024-11-25 14:16:23 +0900

    Fix CMP

--- src/cpu/instruction/cmp_l.rs
+++ src/cpu/instruction/cmp_l.rs
@@ -2,7 +2,7 @@ use crate::cpu::{Cpu, StateType, CCR};
 use anyhow::{bail, Result};
 
 impl Cpu {
-    pub(in super::super)  fn cmp_l(&mut self, opcode: u16) -> Result<u8> {
+    pub(in super::super) fn cmp_l(&mut self, opcode: u16) -> Result<u8> {
         match (opcode >> 8) as u8 {
             0x7a => return self.cmp_l_imm(opcode),
             0x1f => return self.cmp_l_rn(opcode),
@@ -11,48 +11,25 @@ impl Cpu {
     }
 
     fn cmp_l_proc(&mut self, dest: u32, src: u32) -> u32 {
-        let (value, overflowed) = (dest as i32).overflowing_sub(src as i32);
-        if (dest & 0x0fffffff) + (!src & 0x0fffffff) + 1 > 0x0fffffff {
-            self.write_ccr(CCR::H, 1);
-        } else {
-            self.write_ccr(CCR::H, 0);
-        }
+        let (result, overflowed) = dest.overflowing_sub(src);
 
-        if value < 0 {
-            self.write_ccr(CCR::N, 1);
-        } else {
-            self.write_ccr(CCR::N, 0);
-        }
+        self.change_ccr(CCR::H, (dest & 0x0fffffff) < (src & 0x0fffffff));
+        self.change_ccr(CCR::N, (result as i32) < 0);
+        self.change_ccr(CCR::Z, result == 0);
+        self.change_ccr(CCR::V, overflowed);
+        self.change_ccr(CCR::C, dest < src);
 
-        if value == 0 {
-            self.write_ccr(CCR::Z, 1);
-        } else {
-            self.write_ccr(CCR::Z, 0);
-        }
-
-        if overflowed {
-            self.write_ccr(CCR::V, 1);
-        } else {
-            self.write_ccr(CCR::V, 0);
-        }
-
-        if (dest as u64) + (!src as u64) + 1 > 0xffffffff {
-            self.write_ccr(CCR::C, 1);
-        } else {
-            self.write_ccr(CCR::C, 0);
-        }
-
-        value as u32
+        result
     }
 
-     fn cmp_l_imm(&mut self, opcode: u16) -> Result<u8> {
-        let imm = (self.fetch() as u32) << 16 | self.fetch() as u32;
+    fn cmp_l_imm(&mut self, opcode: u16) -> Result<u8> {
         let dest = self.read_rn_l(Cpu::get_nibble_opcode(opcode, 4)?)?;
+        let imm = (self.fetch() as u32) << 16 | self.fetch() as u32;
         self.cmp_l_proc(dest, imm);
         Ok(self.calc_state(StateType::I, 3)?)
     }
 
-     fn cmp_l_rn(&mut self, opcode: u16) -> Result<u8> {
+    fn cmp_l_rn(&mut self, opcode: u16) -> Result<u8> {
         let src = self.read_rn_l(Cpu::get_nibble_opcode(opcode, 3)? & 0x7)?;
         let dest = self.read_rn_l(Cpu::get_nibble_opcode(opcode, 4)?)?;
         self.cmp_l_proc(dest, src);
--- src/cpu/instruction/cmp_w.rs
+++ src/cpu/instruction/cmp_w.rs
@@ -2,7 +2,7 @@ use crate::cpu::{Cpu, StateType, CCR};
 use anyhow::{bail, Result};
 
 impl Cpu {
-    pub(in super::super)  fn cmp_w(&mut self, opcode: u16) -> Result<u8> {
+    pub(in super::super) fn cmp_w(&mut self, opcode: u16) -> Result<u8> {
         match (opcode >> 8) as u8 {
             0x79 => return self.cmp_w_imm(opcode),
             0x1d => return self.cmp_w_rn(opcode),
@@ -11,49 +11,26 @@ impl Cpu {
     }
 
     fn cmp_w_proc(&mut self, dest: u16, src: u16) -> u16 {
-        let (value, overflowed) = (dest as i16).overflowing_sub(src as i16);
-        if (dest & 0x0fff) + (!src & 0x0fff) + 1 > 0x0fff {
-            self.write_ccr(CCR::H, 1);
-        } else {
-            self.write_ccr(CCR::H, 0);
-        }
+        let (result, overflowed) = dest.overflowing_sub(src);
 
-        if value < 0 {
-            self.write_ccr(CCR::N, 1);
-        } else {
-            self.write_ccr(CCR::N, 0);
-        }
+        self.change_ccr(CCR::H, (dest & 0x0fff) < (src & 0x0fff));
+        self.change_ccr(CCR::N, (result as i16) < 0);
+        self.change_ccr(CCR::Z, result == 0);
+        self.change_ccr(CCR::V, overflowed);
+        self.change_ccr(CCR::C, dest < src);
 
-        if value == 0 {
-            self.write_ccr(CCR::Z, 1);
-        } else {
-            self.write_ccr(CCR::Z, 0);
-        }
-
-        if overflowed {
-            self.write_ccr(CCR::V, 1);
-        } else {
-            self.write_ccr(CCR::V, 0);
-        }
-
-        if (dest as u32) + (!src as u32) + 1 > 0xffff {
-            self.write_ccr(CCR::C, 1);
-        } else {
-            self.write_ccr(CCR::C, 0);
-        }
-
-        value as u16
+        result
     }
 
-     fn cmp_w_imm(&mut self, opcode: u16) -> Result<u8> {
+    fn cmp_w_imm(&mut self, opcode: u16) -> Result<u8> {
         let imm = self.fetch();
         let dest = self.read_rn_w(Cpu::get_nibble_opcode(opcode, 4)?)?;
         self.cmp_w_proc(dest, imm);
         Ok(self.calc_state(StateType::I, 2)?)
     }
 
-     fn cmp_w_rn(&mut self, opcode: u16) -> Result<u8> {
-        let src = self.read_rn_w(Cpu::get_nibble_opcode(opcode, 3)? & 0x7)?;
+    fn cmp_w_rn(&mut self, opcode: u16) -> Result<u8> {
+        let src = self.read_rn_w(Cpu::get_nibble_opcode(opcode, 3)?)?;
         let dest = self.read_rn_w(Cpu::get_nibble_opcode(opcode, 4)?)?;
         self.cmp_w_proc(dest, src);
         Ok(self.calc_state(StateType::I, 1)?)
