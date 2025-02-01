afd953330dd7876a34f9c34b8106bebfe9b0535e
Kogepan229 <kogepan25629@gmail.com>
2025-02-01 16:10:19 +0900

    Fix DEC

--- src/cpu/instruction/dec.rs
+++ src/cpu/instruction/dec.rs
@@ -3,107 +3,60 @@ use anyhow::Result;
 
 impl Cpu {
     pub(in super::super) fn dec_b(&mut self, opcode: u16) -> Result<u8> {
-        let result = self.read_rn_b(Cpu::get_nibble_opcode(opcode, 4)?)? - 1;
-        self.write_rn_b(Cpu::get_nibble_opcode(opcode, 4)?, result)?;
-        if (result as i8) < 0 {
-            self.write_ccr(CCR::N, 1);
-        } else {
-            self.write_ccr(CCR::N, 0);
-        }
-        if result == 0 {
-            self.write_ccr(CCR::Z, 1);
-        } else {
-            self.write_ccr(CCR::Z, 0);
-        }
-        if result == 0x7f {
-            self.write_ccr(CCR::V, 1);
-        } else {
-            self.write_ccr(CCR::V, 0);
-        }
+        let rd_i = Cpu::get_nibble_opcode(opcode, 4)?;
+        let src = self.read_rn_w(rd_i)? as i8;
+        let result = src - 1;
+
+        self.write_rn_b(rd_i, result as u8)?;
+        self.change_ccr(CCR::N, result < 0);
+        self.change_ccr(CCR::Z, result == 0);
+        self.change_ccr(CCR::V, result == 0x7f);
         Ok(self.calc_state(StateType::I, 1)?)
     }
 
     pub(in super::super) fn dec_w_1(&mut self, opcode: u16) -> Result<u8> {
-        let result = self.read_rn_w(Cpu::get_nibble_opcode(opcode, 4)?)? - 1;
-        self.write_rn_w(Cpu::get_nibble_opcode(opcode, 4)?, result)?;
-        if (result as i16) < 0 {
-            self.write_ccr(CCR::N, 1);
-        } else {
-            self.write_ccr(CCR::N, 0);
-        }
-        if result == 0 {
-            self.write_ccr(CCR::Z, 1);
-        } else {
-            self.write_ccr(CCR::Z, 0);
-        }
-        if result == 0x7fff {
-            self.write_ccr(CCR::V, 1);
-        } else {
-            self.write_ccr(CCR::V, 0);
-        }
+        let rd_i = Cpu::get_nibble_opcode(opcode, 4)?;
+        let src = self.read_rn_w(rd_i)? as i16;
+        let result = src - 1;
+
+        self.write_rn_w(rd_i, result as u16)?;
+        self.change_ccr(CCR::N, result < 0);
+        self.change_ccr(CCR::Z, result == 0);
+        self.change_ccr(CCR::V, result == 0x7fff);
         Ok(self.calc_state(StateType::I, 1)?)
     }
 
     pub(in super::super) fn dec_w_2(&mut self, opcode: u16) -> Result<u8> {
-        let result = self.read_rn_w(Cpu::get_nibble_opcode(opcode, 4)?)? - 1;
-        self.write_rn_w(Cpu::get_nibble_opcode(opcode, 4)?, result)?;
-        if (result as i16) < 0 {
-            self.write_ccr(CCR::N, 1);
-        } else {
-            self.write_ccr(CCR::N, 0);
-        }
-        if result == 0 {
-            self.write_ccr(CCR::Z, 1);
-        } else {
-            self.write_ccr(CCR::Z, 0);
-        }
-        if result == 0x7ffe || result == 0x7fff {
-            self.write_ccr(CCR::V, 1);
-        } else {
-            self.write_ccr(CCR::V, 0);
-        }
+        let rd_i = Cpu::get_nibble_opcode(opcode, 4)?;
+        let src = self.read_rn_w(rd_i)? as i16;
+        let result = src - 2;
+
+        self.write_rn_w(rd_i, result as u16)?;
+        self.change_ccr(CCR::N, result < 0);
+        self.change_ccr(CCR::Z, result == 0);
+        self.change_ccr(CCR::V, result == 0x7ffe || result == 0x7fff);
         Ok(self.calc_state(StateType::I, 1)?)
     }
 
     pub(in super::super) fn dec_l_1(&mut self, opcode: u16) -> Result<u8> {
-        let result = self.read_rn_l(Cpu::get_nibble_opcode(opcode, 4)?)? - 1;
-        self.write_rn_l(Cpu::get_nibble_opcode(opcode, 4)?, result)?;
-        if (result as i32) < 0 {
-            self.write_ccr(CCR::N, 1);
-        } else {
-            self.write_ccr(CCR::N, 0);
-        }
-        if result == 0 {
-            self.write_ccr(CCR::Z, 1);
-        } else {
-            self.write_ccr(CCR::Z, 0);
-        }
-        if result == 0x7fffffff {
-            self.write_ccr(CCR::V, 1);
-        } else {
-            self.write_ccr(CCR::V, 0);
-        }
+        let rd_i = Cpu::get_nibble_opcode(opcode, 4)?;
+        let src = self.read_rn_l(rd_i)? as i32;
+        let result = src - 1;
+        self.write_rn_l(Cpu::get_nibble_opcode(opcode, 4)?, result as u32)?;
+        self.change_ccr(CCR::N, result < 0);
+        self.change_ccr(CCR::Z, result == 0);
+        self.change_ccr(CCR::V, result == 0x7fffffff);
         Ok(self.calc_state(StateType::I, 1)?)
     }
 
     pub(in super::super) fn dec_l_2(&mut self, opcode: u16) -> Result<u8> {
-        let result = self.read_rn_l(Cpu::get_nibble_opcode(opcode, 4)?)? - 1;
-        self.write_rn_l(Cpu::get_nibble_opcode(opcode, 4)?, result)?;
-        if (result as i32) < 0 {
-            self.write_ccr(CCR::N, 1);
-        } else {
-            self.write_ccr(CCR::N, 0);
-        }
-        if result == 0 {
-            self.write_ccr(CCR::Z, 1);
-        } else {
-            self.write_ccr(CCR::Z, 0);
-        }
-        if result == 0x7ffffffe || result == 0x7fffffff {
-            self.write_ccr(CCR::V, 1);
-        } else {
-            self.write_ccr(CCR::V, 0);
-        }
+        let rd_i = Cpu::get_nibble_opcode(opcode, 4)?;
+        let src = self.read_rn_l(rd_i)? as i32;
+        let result = src - 2;
+        self.write_rn_l(Cpu::get_nibble_opcode(opcode, 4)?, result as u32)?;
+        self.change_ccr(CCR::N, result < 0);
+        self.change_ccr(CCR::Z, result == 0);
+        self.change_ccr(CCR::V, result == 0x7ffffffe || result == 0x7fffffff);
         Ok(self.calc_state(StateType::I, 1)?)
     }
 }
